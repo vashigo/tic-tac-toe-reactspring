85c000a00d46a229197762cd5c16627b30bb67d3
Andres Vasquez <andres.vasquez@hydra.cloud>
2020-07-06 19:18:12 -0500

    initial tictactoe

---
+++ src/main/java/edu/escuelaing/arsw/tictactoeReact/TictactoeReactApplication.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arsw.tictactoeReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictactoeReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TictactoeReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/escuelaing/arsw/tictactoeReact/TictactoeReactApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arsw.tictactoeReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictactoeReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
